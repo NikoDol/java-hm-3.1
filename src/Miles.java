0c4ef6bfc031aaf2954dbdb3894245711ba0bef7
NikoDol <69104440+NikoDol@users.noreply.github.com>
2020-10-01 15:00:14 +0500

    Update Miles.java

--- src/Miles.java
+++ src/Miles.java
@@ -1,8 +1,8 @@
 public class Miles {
     public static void main(String[] args) {
-        double PriceTicket = 25437.82;
+        double priceTicket = 25437.82;
         // За каждые 20 рублей , одна миля.
-        int miles = (int)PriceTicket/20;
+        int miles = (int)priceTicket/20;
         System.out.println(miles);
     }
 }
