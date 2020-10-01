d5b91bc5535fb60d837cf0fa1abb539c9f7e9f8b
Nikolai Dolgachev <dolgachev.nik@gmail.com>
2020-10-01 13:04:03 +0500

    first commit

---
+++ src/Miles.java
@@ -0,0 +1,8 @@
+public class Miles {
+    public static void main(String[] args) {
+        double PriceTicket = 25437.82;
+        // За каждые 20 рублей , одна миля.
+        int miles = (int)PriceTicket/20;
+        System.out.println(miles);
+    }
+}
